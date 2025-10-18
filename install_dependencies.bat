93512b16f6928d06e89c57c3b1f23928fbf6fe02
huuthanh2k4 <143076709+huuthanh2k4@users.noreply.github.com>
2025-10-18 22:50:36 +0700

    cập nhập

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -30,7 +30,6 @@ echo.
 echo ===================================================================
 echo  CAI DAT HOAN TAT!
 echo  Tu bay gio, ban chi can chay file "start_app.bat" de khoi dong.
-echo  Cua so nay se tu dong dong sau 5 giay...
 echo ===================================================================
 echo.
 pause
